ee1173ff722a72add32ad5c2c896d06cf1bd4c95
Michael Schultz <mikewschultz9029@gmail.com>
2023-02-14 09:29:42 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,98 @@
+// Example of What your JS File in VS Code should look like
+
+
+/*
+    Remember!!! Once you’ve completed a problem,
+    COMMENT OUT YOUR WORK before moving on to the next one.
+    This makes it easier to read and debug the current
+    Solution you are working on.
+*/
+
+
+// I. Variables & Data Types
+// A. Q + A
+// B. Strings
+// C. Booleans
+// D. The Farm
+// E. Driver's Ed
+//_____________________________________________________________
+
+
+
+
+
+
+// II. Loops
+// A. The Basics
+// B. Get even
+// C. Give me Five
+// D. Savings account
+//_____________________________________________________________
+
+
+
+
+
+
+
+
+// III. Arrays & Control Flow
+// A. Talk about it:
+// B. Easy Does It
+// C. Accessing elements
+// D. Change values
+// E. Mix It Up
+// F. Biggie Smalls
+// G. Monkey in the Middle
+// H. What's in Your Closet?
+//_____________________________________________________________
+
+
+
+
+
+
+
+
+// IV. Functions
+// A.
+// B.
+// C.
+// D.
+// E.
+// F.
+// G.
+// H.
+//_____________________________________________________________
+
+
+
+
+
+
+// V. Objects
+// A.
+// B.
+// C.
+// D.
+// E.
+// F.
+// G.
+//_____________________________________________________________
+
+
+// Extra
+
+
+
+// Resources to help you along the way:
+
+// MDN JavaScript => Here is a video showing you how to navigate through the documentation once you click on the link.
+
+//     Functions - Eloquent JavaScript
+
+// Array Methods - W3Schools
+
+// JS Modules - W3Schools
+
+// Loops & Iterations
